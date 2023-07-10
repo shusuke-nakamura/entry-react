879b78f22ef968085bd412e9851357da7d274fe4
Shusuke Nakamura <shusuke.nakamura@systemi.com>
2023-07-10 18:15:29 +0900

    4-2まで学習完了

--- react_app/src/App_4-1.js
+++ react_app/src/App_4-1.js
@@ -2,21 +2,14 @@ import React, {Component, useState} from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const clickFunc = ()=> {
-    setCount(count + 1)
-  }
-
+  const [message] = useState("Welcome to Hooks!")
   return (
     <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
         <div className="alert alert-primary text-content">
-          <p className="h5 mb-3">click: {count} times!</p>
-          <div>
-            <button className="btn btn-primary" onClick={clickFunc}>Click me</button>
-          </div>
+          <p className="h5">{message}.</p>
         </div>
       </div>
     </div>
